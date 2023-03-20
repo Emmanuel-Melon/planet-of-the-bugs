0b28b1a8783e419c042805b01b05dcc798518c8a
Eman <emmanuelgatwech@gmail.com>
2023-03-21 00:13:00 +0300

    feat: extract apollo config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,29 +3,6 @@
 
   import {onMount} from 'svelte';
   import { PUBLIC_HASURA_ADMIN_SECRET } from "$env/static/public";
-  import {
-    ApolloClient,
-    InMemoryCache,
-    gql,
-    createHttpLink,
-    HttpLink,
-  // @ts-ignore
-  } from "@apollo/client/core/core.cjs";
-  import { setClient, query, mutation } from "svelte-apollo";
-
-  const httpLink = new HttpLink({
-    uri: "https://planet-of-the-bugs.hasura.app/v1/graphql",
-    credentials: "include",
-    headers: {
-      "x-hasura-admin-secret": PUBLIC_HASURA_ADMIN_SECRET,
-    },
-  });
-
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    credentials: "include",
-    link: httpLink,
-  });
 
   setClient(client);
 
