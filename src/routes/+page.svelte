a916dffbfc66ddaebec0291dd53e2a315de561b0
Maged Faiz <magedfaiz98@gmail.com>
2023-03-22 12:59:51 +0200

    feat: Refactored Course Card to a component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
   import { onMount } from "svelte";
   import { FETCH_COURSES } from "$lib/queries/courses";
   import { query, mutation } from "svelte-apollo";
+  import CourseCard from "../components/CourseCard.svelte";
 
   const courses = query(FETCH_COURSES);
 
@@ -19,15 +20,10 @@
 
 <div class="text-column">
   <h1>Planet of The Bugs</h1>
-  <div class="list">
+  <div class="grid place-items-center grid-cols-1 mt-8 gap-8 lg:grid-cols-2">
     {#if $courses.data}
       {#each $courses.data.courses as course}
-        <div class="course">
-          <h3><a  class="link" href={`/courses/${course.slug}`}>{course.name}</a></h3>
-          <p>Complexity: {course.complexity}</p>
-          <p>{course.description}</p>
-          <p>Creator: {course.creator}</p>
-        </div>
+      <CourseCard course={course} />
       {/each}
     {:else}
       <p>Loading...</p>
