9a0fae53a3ea8e928ad7be6ba90cd753b6357889
Eman <emmanuelgatwech@gmail.com>
2023-03-26 18:12:50 +0300

    feat: add auth credentials

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,13 @@
-import { SvelteKitAuth } from "@auth/sveltekit"
-import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import { SvelteKitAuth } from "@auth/sveltekit";
+import GitHub from "@auth/core/providers/github";
+import { PRIVATE_AUTH_SECRET, PRIVATE_GITHUB_ID, PRIVATE_GITHUB_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
-})
+  providers: [
+    GitHub({
+      clientId: PRIVATE_GITHUB_ID,
+      clientSecret: PRIVATE_GITHUB_SECRET,
+    }),
+  ],
+  secret: PRIVATE_AUTH_SECRET,
+});
