fed68ebcefa36197216ca39a2e0b6c63f4aa6587
Eman <emmanuelgatwech@gmail.com>
2023-03-28 21:22:39 +0300

    feat: add auth callbacks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -21,5 +21,57 @@ export const handle = SvelteKitAuth({
     }),
   ],
   secret: PRIVATE_AUTH_SECRET,
-  trustHost: true
+  trustHost: true,
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return {
+        user,
+        account,
+        profile,
+        email,
+      };
+    },
+    async session(data) {
+      console.log("got data");
+      console.log(data);
+
+      const { session, token } = data;
+  
+      // custom jwt
+      // const encodedToken = jwt.sign({ ...token?.user }, jwtSecret, { algorithm: 'HS256'});
+      // session.accessToken = token.accessToken;
+      // session.user.id = token.id;
+      // session.jwt = encodedToken;
+      // pick certain fields
+      // session.user = token?.user;
+      // session.id_token = token.id_token
+      return session;
+    },
+    async jwt(data) {
+      const { token, user, account, profile, isNewUser } = data;
+      if (account) {
+        
+        // token.accessToken = account.access_token
+        // token.id = profile.id
+        // token.id_token = account.id_token
+        console.log("got account");
+        console.log(account);
+      }
+
+      if(user) {
+        // token.user = user
+        console.log("got user");
+        console.log(user);
+      }
+
+
+      if(profile) {
+        // token.profile = profile
+        console.log("got profile");
+        console.log(profile);
+      }
+      return token;
+    },
+  },
+  debug: true,
 });
