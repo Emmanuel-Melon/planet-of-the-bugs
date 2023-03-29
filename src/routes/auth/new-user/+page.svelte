136f7dfa01954b8bdcd5f49acb3e9b2fe62f9bf4
Maged Faiz <magedfaiz98@gmail.com>
2023-03-29 16:44:27 +0200

    feat: Added form validation

--- src/routes/auth/new-user/+page.svelte
+++ src/routes/auth/new-user/+page.svelte
@@ -5,11 +5,14 @@
     import welcome from '$lib/images/svelte-welcome.png'
     import { ADD_NEW_USER } from "$lib/mutations/users";
     import { onMount } from "svelte";
-  import { mutation } from "svelte-apollo";
+    import { mutation } from "svelte-apollo";
 
     let username = '';
     let name = '';
     let email = '';
+
+    let usernamePattern = new RegExp(/^[a-zA-Z][a-zA-Z0-9_-]{3,19}$/);
+    let emailPattern = new RegExp(/[a-zA-Z0-9_\\.\\+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-\\.]+/)
 
     onMount( () => {
     page.subscribe((value) => {
@@ -22,8 +25,11 @@
 
     const addNewUser = mutation(ADD_NEW_USER)
 
-    async function handleSubmit() {
-        email.toLowerCase
+    async function handleSubmit() {        
+        if(!usernamePattern.test(username) && !emailPattern.test(email)){
+            return
+        }
+
         try {
             const result = await addNewUser({
                 variables: {
@@ -60,7 +66,9 @@
             <input 
                 id="username" type="text" 
                 bind:value={username} 
-                on:input={(event)=> username = event.target.value.toLowerCase()} 
+                on:input={(event)=> username = event.target.value.toLowerCase()}
+                pattern={"^[a-zA-Z][a-zA-Z0-9_-]{3,19}$"}
+                title="Must start with a letter, have at least 4 characters and contain only letters, numbers, hyphens, or underscores." 
                 class="input input-bordered w-full max-w-xs"
                 required
                 />
@@ -85,10 +93,11 @@
                 id="email" 
                 type="email" 
                 bind:value={email} 
+                on:input={(event)=> email = event.target.value.toLowerCase()}
                 class="input input-bordered w-full max-w-xs" 
                 required
                 />
         </div>
-        <button class="btn btn-primary" on:click|preventDefault={handleSubmit}>Submit</button>
+        <button class="btn btn-primary" on:click={handleSubmit}>Submit</button>
     </form>
 </div>
