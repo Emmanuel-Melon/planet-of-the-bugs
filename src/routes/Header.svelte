c0d7b99478fed523e90151255f4abbf1b6e4aef5
Maged Faiz <magedfaiz98@gmail.com>
2023-04-01 15:52:26 +0200

    feat: Added Navbar structure

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,16 +2,54 @@
   import { page } from "$app/stores";
   import logo from "$lib/images/svelte-logo.svg";
   import github from "$lib/images/github.svg";
+  import "iconify-icon";
+
+
+  $: current = $page.url.pathname
+
 </script>
 
-<header>
-  <div class="corner">
-    <a href="https://kit.svelte.dev">
-      <img src={logo} alt="SvelteKit" />
-    </a>
-  </div>
+<header class="navbar bg-base-100">
+    <div class="navbar-start">
+      <a href='/' class="btn btn-ghost normal-case text-xl">Planet Of The Bugs</a>
+    </div>
 
-  <nav>
+    <ul class="navbar-center hidden lg:flex justify-between items-center w-1/3 text-lg font-bold">
+      <li 
+      class={current === '/' ?
+       "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline underline"
+        : "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline"}
+        >
+        <a href="/">Home</a>
+      </li>
+      <li 
+      class={current === '/courses' ?
+       "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline underline"
+        : "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline"}
+        >
+        <a href="/courses">Courses</a>
+      </li>
+      <li 
+      class={current === '/about' ?
+       "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline underline"
+        : "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline"}
+        >
+        <a href="/about">About</a>
+      </li>
+    </ul>
+
+    <div class="navbar-end">
+       <button class="lg:hidden block text-3xl btn-ghost">
+        <iconify-icon icon="heroicons:bars-3-bottom-right" />
+       </button>
+
+       <button class="lg:block hidden text-3xl btn-ghost mr-4">
+        <iconify-icon icon="heroicons:user-solid" />
+       </button>
+    </div>
+  
+
+  <!-- <nav>
     <svg viewBox="0 0 2 3" aria-hidden="true">
       <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
     </svg>
@@ -29,101 +67,6 @@
     <svg viewBox="0 0 2 3" aria-hidden="true">
       <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
     </svg>
-  </nav>
+  </nav> -->
 
-  <div class="corner">
-    <a href="https://github.com/Emmanuel-Melon/planet-of-the-bugs">
-      <img src={github} alt="GitHub" />
-    </a>
-  </div>
 </header>
-
-<style>
-  header {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .corner {
-    width: 3em;
-    height: 3em;
-  }
-
-  .corner a {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-  }
-
-  .corner img {
-    width: 2em;
-    height: 2em;
-    object-fit: contain;
-  }
-
-  nav {
-    display: flex;
-    justify-content: center;
-    --background: rgba(255, 255, 255, 0.7);
-  }
-
-  svg {
-    width: 2em;
-    height: 3em;
-    display: block;
-  }
-
-  path {
-    fill: var(--background);
-  }
-
-  ul {
-    position: relative;
-    padding: 0;
-    margin: 0;
-    height: 3em;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    list-style: none;
-    background: var(--background);
-    background-size: contain;
-  }
-
-  li {
-    position: relative;
-    height: 100%;
-  }
-
-  li[aria-current="page"]::before {
-    --size: 6px;
-    content: "";
-    width: 0;
-    height: 0;
-    position: absolute;
-    top: 0;
-    left: calc(50% - var(--size));
-    border: var(--size) solid transparent;
-    border-top: var(--size) solid var(--color-theme-1);
-  }
-
-  nav a {
-    display: flex;
-    height: 100%;
-    align-items: center;
-    padding: 0 0.5rem;
-    color: var(--color-text);
-    font-weight: 700;
-    font-size: 0.8rem;
-    text-transform: uppercase;
-    letter-spacing: 0.1em;
-    text-decoration: none;
-    transition: color 0.2s linear;
-  }
-
-  a:hover {
-    color: var(--color-theme-1);
-  }
-</style>
