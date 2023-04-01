9de22ce483c8188d64377c8ab73a3701efd417a3
Maged Faiz <magedfaiz98@gmail.com>
2023-04-01 22:03:20 +0200

    feat: fully responsive and with signing options

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,11 +1,30 @@
 <script>
-  import { page } from "$app/stores";
-  import logo from "$lib/images/svelte-logo.svg";
-  import github from "$lib/images/github.svg";
+// @ts-nocheck
+
+  import { page } from '$app/stores';
+  import { onMount, onDestroy } from 'svelte';
+  import { signOut } from '@auth/sveltekit/client';
   import "iconify-icon";
 
 
+  let user;
   $: current = $page.url.pathname
+  $: menuLinks = [
+    { text: 'Home', path: '/' },
+    { text: 'Courses', path: '/courses' },
+    { text: 'About', path: '/about' }
+  ];
+
+  onMount(() => {
+    user = $page.data.session?.user;
+  });
+
+  onDestroy(() => {
+    user = null;
+    current = null;
+    isMenuOpen = false;
+    menuLinks = [];
+  });
 
 </script>
 
@@ -14,59 +33,54 @@
       <a href='/' class="btn btn-ghost normal-case text-xl">Planet Of The Bugs</a>
     </div>
 
-    <ul class="navbar-center hidden lg:flex justify-between items-center w-1/3 text-lg font-bold">
-      <li 
-      class={current === '/' ?
-       "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline underline"
-        : "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline"}
-        >
-        <a href="/">Home</a>
-      </li>
-      <li 
-      class={current === '/courses' ?
-       "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline underline"
-        : "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline"}
-        >
-        <a href="/courses">Courses</a>
-      </li>
-      <li 
-      class={current === '/about' ?
-       "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline underline"
-        : "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline"}
-        >
-        <a href="/about">About</a>
-      </li>
+    <ul class="navbar-center hidden lg:flex justify-between items-center w-1/3 text-lg font-bold ml-8">
+      {#each menuLinks as { text, path }}
+        <li class={path === current ? "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline underline" : "hidden lg:flex transition-all underline-offset-8 decoration-primary hover:underline"}>
+          <a href={path}>{text}</a>
+        </li>
+      {/each}
     </ul>
 
-    <div class="navbar-end">
-       <button class="lg:hidden block text-3xl btn-ghost">
-        <iconify-icon icon="heroicons:bars-3-bottom-right" />
-       </button>
+    <div class="navbar-end relative">
+      <div class="dropdown dropdown-end lg:hidden">
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <div tabindex="0" class="btn btn-ghost btn-circle text-3xl">
+          <iconify-icon icon="heroicons:bars-3-bottom-right-20-solid" />
+        </div>
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+          {#each menuLinks as { text, path }}
+            <li>
+              <a href={path}>{text}</a>
+            </li>
+          {/each}
+          <li>
+            {#if user}
+              <button on:click|preventDefault={() => signOut()}>Sign Out</button>
+            {:else}
+              <a href="/auth">Sign In</a>
+            {/if}
+          </li>
+        </ul>
+      </div>
 
-       <button class="lg:block hidden text-3xl btn-ghost mr-4">
-        <iconify-icon icon="heroicons:user-solid" />
-       </button>
+    <div class="dropdown dropdown-end lg:block hidden text-3xl btn-ghost mr-4">
+      {#if user}
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <div tabindex="0" class="btn btn-ghost btn-circle avatar">
+          <div class="w-10 rounded-full">
+            <img src={user.image} alt={user.name} />
+          </div>
+        </div>
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+          <li>
+            <button on:click|preventDefault={() => signOut()}>Sign Out</button>
+          </li>
+        </ul>
+      {:else}
+        <a href="/auth" class="btn btn-outline btn-primary">Sign In</a>
+      {/if}
     </div>
-  
-
-  <!-- <nav>
-    <svg viewBox="0 0 2 3" aria-hidden="true">
-      <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-    </svg>
-    <ul>
-      <li aria-current={$page.url.pathname === "/" ? "page" : undefined}>
-        <a href="/">Home</a>
-      </li>
-      <li aria-current={$page.url.pathname === "/courses" ? "page" : undefined}>
-        <a href="/courses">Courses</a>
-      </li>
-      <li aria-current={$page.url.pathname === "/about" ? "page" : undefined}>
-        <a href="/about">About</a>
-      </li>
-    </ul>
-    <svg viewBox="0 0 2 3" aria-hidden="true">
-      <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-    </svg>
-  </nav> -->
-
+    </div>
 </header>
