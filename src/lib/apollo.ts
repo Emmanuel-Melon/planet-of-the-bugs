6bdb0dcdfb0deb91db565503dd64c2b296801b4e
Eman <emmanuelgatwech@gmail.com>
2023-04-04 16:18:20 +0300

    fix: resolve apollo client imports

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,11 +1,8 @@
 import { PUBLIC_HASURA_ADMIN_SECRET } from "$env/static/public";
-import {
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink,
-  HttpLink,
-  // @ts-ignore
-} from "@apollo/client/core/core.cjs";
+
+import { ApolloClient } from "@apollo/client/core";
+import { HttpLink } from "@apollo/client/link/http";
+import { InMemoryCache } from "@apollo/client/cache";
 
 const httpLink = new HttpLink({
   uri: "https://planet-of-the-bugs.hasura.app/v1/graphql",
