13e2764ba799aec982606e63e8ad4a30be110a33
Maged Faiz <magedfaiz98@gmail.com>
2023-04-04 19:45:17 +0200

    finalizing pr

--- src/routes/courses/[slug=slug]/[lesson]/+page.svelte
+++ src/routes/courses/[slug=slug]/[lesson]/+page.svelte
@@ -28,19 +28,23 @@
 </script>
 
 <section
-  class="border border-#efefef rounded-sm card bg-base-100 shadow-md"
+  class="border border-#efefef rounded-sm card bg-base-100 shadow-md box-border"
 >
   <div class="flex">
-    <div class="flex-1 p-4">
+    
+    <div class={isExpanded ? "hidden" : "flex-1 p-4"}>
       {#if $course.data}
         <LessonOverview lesson={$course.data.lessons_by_pk} />
       {:else}
         <p>Loading...</p>
       {/if}
     </div>
-    <div class="flex-1">
-      <CodeEditor handleExpand={handleExpand}/>
+
+    <div class="flex-1 w-full">
+      <CodeEditor handleExpand={handleExpand} isExpanded={isExpanded}/>
+      {#if !isExpanded}
       <CodeOutput />
+      {/if}
     </div>
   </div>
 
