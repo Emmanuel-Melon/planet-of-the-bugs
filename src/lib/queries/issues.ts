80a631a9fb3d137b51e6c8f557d0ee92036e46c8
Eman <emmanuelgatwech@gmail.com>
2023-04-17 20:50:36 +0300

    feat: add Github GraphQL API queries

---
+++ src/lib/queries/issues.ts
@@ -0,0 +1,24 @@
+import {
+  gql,
+  // @ts-ignore
+} from "@apollo/client/core/core.cjs";
+
+export const FETCH_ISSUES_BY_DIFFICULTY = gql`
+  query MyQuery {
+    search(
+      query: "label:good-first-issue label:help-wanted label:bug is:open"
+      type: ISSUE
+      first: 10
+    ) {
+      edges {
+        node {
+          ... on Issue {
+            id
+            title
+            url
+          }
+        }
+      }
+    }
+  }
+`;
