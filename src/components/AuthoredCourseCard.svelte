113c8acb096d03de7053439221753c8aa758c37c
Eman <emmanuelgatwech@gmail.com>
2023-04-30 11:03:36 +0300

    feat: theme changes

--- src/components/AuthoredCourseCard.svelte
+++ src/components/AuthoredCourseCard.svelte
@@ -1,16 +1,20 @@
 <script>
     // @ts-nocheck
     export let course;
-
+    import "iconify-icon";
   </script>
   
-  <div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card w-96 bg-gray-100 shadow-lg">
     <div class="card-body">
       <h3 class="card-title">
         <a class="link" href={`/courses/authored/${course.slug}`}>{course.title}</a>
       </h3>
-      <p>Complexity: {course.complexity}</p>
+      <div class="badge badge-accent gap-2">
+        <iconify-icon icon="heroicons:star" />
+        {course.complexity}
+      </div>
       <p>{course.description}</p>
     </div>
   </div>
   
+
