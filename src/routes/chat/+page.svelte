24b52974f5364db168537dc2b9c2381d62a56a1b
Eman <emmanuelgatwech@gmail.com>
2023-05-10 21:14:37 +0300

    feat: added chat page

---
+++ src/routes/chat/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  import "iconify-icon";
+</script>
+
+<section class="bg-white w-3/4 mx-auto">
+  <div class="flex p-4">
+    <div>
+      <h1>
+        <iconify-icon
+          icon="heroicons:chat-bubble-bottom-center-text-20-solid"
+        />
+        Chat
+      </h1>
+    </div>
+  </div>
+  <div class="flex">
+    <div class="p-4 basis-1/4">
+      <h3>Message List</h3>
+    </div>
+    <div class="flex basis-3/4 justify-center items-center p-4">
+        <div>
+            <h3>Select a Conversation.</h3>
+            <div class="divider">Or</div>
+            <button>Message Someone</button>
+        </div>
+    </div>
+  </div>
+</section>
