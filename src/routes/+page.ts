3f7706443fa76d63ed5841ddaebdfd9e8a9af141
Eman <emmanuelgatwech@gmail.com>
2023-05-12 20:11:35 +0300

    feat: disable pre-rendering of +page.ts in root

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,25 +1,15 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
-
-import {
-  GITHUB_USER_BASIC_INFO,
-  REPOS_CONTRIBUTED_TO,
-  GET_PINNED_ITEMS
-} from '$lib/queries/user';
 import { GITHUB_API } from "$lib/apollo";
 
 export const load = async (event) => {
 
-  const { params, url, setHeaders, route, parent, fetch, depends, data: pageData } = event;
+  const { parent } = event;
 
   const { session } = await parent();
   if (session?.token !== null || session?.token !== undefined) {
     GITHUB_API.setSession(session?.token?.accessToken);
+    const githubClient = GITHUB_API.getGithubClient();
+    return {
+      session
+    };
   }
-  const githubClient = GITHUB_API.getGithubClient();
-  return {
-
-
-  };
 };
