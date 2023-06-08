dccbd15e16bae1b91838642b157530cb2ffd4a00
Maged Faiz <magedfaiz98@gmail.com>
2023-06-08 11:36:40 +0200

    optimized explore & profile pages

--- apps/client/src/components/Repositories/RepositoryRecommendations.svelte
+++ apps/client/src/components/Repositories/RepositoryRecommendations.svelte
@@ -6,7 +6,7 @@
   // console.log(user);
 </script>
 
-<div class="flex gap-2 basis-4/5 flex-wrap">
+<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 basis-4/5">
   {#each repositories as edge}
     <RepoOverviewCard repo={edge?.node} {user} />
   {/each}
