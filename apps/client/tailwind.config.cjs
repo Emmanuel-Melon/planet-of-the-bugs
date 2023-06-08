850860cff1fd7b179f27f49ae1b15d9a5f68037b
Eman <emmanuelgatwech@gmail.com>
2023-06-08 16:42:20 +0300

    feat: change fonts and theme

--- apps/client/tailwind.config.cjs
+++ apps/client/tailwind.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   daisyui: {
-    themes: ["lofi"],
+    themes: ["emerald"],
   },
   plugins: [require("daisyui"), require("@tailwindcss/typography")],
 };
