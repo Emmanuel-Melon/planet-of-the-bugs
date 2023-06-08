5ca71ce97bef4b2d1d475f6bd1c7ead0a46e00fa
Eman <emmanuelgatwech@gmail.com>
2023-06-08 17:11:40 +0300

    feat: modify header

--- apps/client/tailwind.config.cjs
+++ apps/client/tailwind.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   daisyui: {
-    themes: ["emerald"],
+    themes: ["lofi"],
   },
   plugins: [require("daisyui"), require("@tailwindcss/typography")],
 };
