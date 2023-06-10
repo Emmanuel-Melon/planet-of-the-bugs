f8b60af9f36af2cd9bc77321bd1593e6cdd21f25
Eman <emmanuelgatwech@gmail.com>
2023-06-10 14:52:41 +0300

    feat: add page loader component

--- packages/svelte-ui/index.ts
+++ packages/svelte-ui/index.ts
@@ -1,2 +1,3 @@
-export { default as Tabs } from './components/Tabs.svelte';
 export { default as MyCounterButton } from './components/MyCounterButton.svelte';
+export { default as Pageloader } from './components/Pageloader.svelte';
+export { default as Tabs } from './components/Tabs.svelte';
