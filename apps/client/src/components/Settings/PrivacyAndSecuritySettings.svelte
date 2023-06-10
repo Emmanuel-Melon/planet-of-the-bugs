1eac390615f42621f70c5aacc2d72656222330ac
Eman <emmanuelgatwech@gmail.com>
2023-06-10 20:22:38 +0300

    feat: refactor settings page

---
+++ apps/client/src/components/Settings/PrivacyAndSecuritySettings.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { Card } from "svelte-ui";
+</script>
+
+<div class="basis-3/4 space-y-4">
+  <Card>
+    <div class="card-body">
+      <h3 class="card-title">Privacy &amp; Security</h3>
+    </div>
+  </Card>
+</div>
