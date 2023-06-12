3de0811def3311427b40d3ce442f69137be3cf86
Eman <emmanuelgatwech@gmail.com>
2023-06-12 15:06:41 +0300

    feat: change footer links

--- apps/client/src/routes/+layout.svelte
+++ apps/client/src/routes/+layout.svelte
@@ -34,11 +34,11 @@
   <main class="space-y-4">
     <slot />
   </main>
-  <footer class="flex items-center justify-center p-2 bg-base-200">
-    <div class="flex gap-2">
-      <a class="link" target="_blank" href="https://support.planetofthebugs.xyz">Support</a>
-      <a class="link" target="_blank" href="https://github.com/Emmanuel-Melon/planet-of-the-bugs">Contribute</a>
-      <a class="link" href="/about">About</a>
+  <footer class="flex items-center justify-center p-4 bg-base-200">
+    <div class="flex gap-4">
+      <a class="link link-hover flex items-center gap-2" target="_blank" href="https://support.planetofthebugs.xyz"><iconify-icon icon="ri:customer-service-2-line" /> Contact Support</a>
+      <a class="link link-hover flex items-center gap-2" target="_blank" href="https://github.com/Emmanuel-Melon/planet-of-the-bugs"><iconify-icon icon="ri:send-plane-2-line" /> Invite a friend</a>
+      <a class="link link-hover flex items-center gap-2" href="/about"><iconify-icon icon="ri:information-line" /> About</a>
     </div>
   </footer>
 </div>
