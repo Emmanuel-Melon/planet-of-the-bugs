ac3448aae613622c92760ad31af24dc50f7ff04a
Maged Faiz <magedfaiz98@gmail.com>
2023-07-30 10:46:57 +0200

    feat: improved lesson navigation

--- apps/client/src/routes/courses/[slug]/[id]/navigationStore.ts
+++ apps/client/src/routes/courses/[slug]/[id]/navigationStore.ts
@@ -32,8 +32,41 @@ export function updateNavigationStore(
   // Index database field start at 1
   const currentChapterIndex = currentChapter.index - 1;
   const currentLessonIndex = currentLesson.index - 1;
-  const nextLesson = lessons[currentLessonIndex + 1] || null;
-  const previousLesson = lessons[currentLessonIndex - 1] || null;
+
+  let nextLesson = null;
+  let previousLesson = null;
+
+  // Check if there's a next lesson in the current chapter
+  if (currentLessonIndex + 1 < lessons.length) {
+    nextLesson = lessons[currentLessonIndex + 1];
+  } else {
+    // If there's no next lesson in the current chapter, move to the next chapter
+    const nextChapterIndex = currentChapterIndex + 1;
+
+    // Check if there's a next chapter available
+    if (nextChapterIndex < chapters.length) {
+      const nextChapter = chapters[nextChapterIndex];
+      nextLesson =
+        nextChapter.lessons.length > 0 ? nextChapter.lessons[0] : null;
+    }
+  }
+
+  // Check if there's a previous lesson in the current chapter
+  if (currentLessonIndex > 0) {
+    previousLesson = lessons[currentLessonIndex - 1];
+  } else {
+    // If there's no previous lesson in the current chapter, move to the previous chapter
+    const previousChapterIndex = currentChapterIndex - 1;
+
+    // Check if there's a previous chapter available
+    if (previousChapterIndex >= 0) {
+      const previousChapter = chapters[previousChapterIndex];
+      previousLesson =
+        previousChapter.lessons.length > 0
+          ? previousChapter.lessons[previousChapter.lessons.length - 1]
+          : null;
+    }
+  }
 
   navigationStore.set({
     currentChapterIndex,
