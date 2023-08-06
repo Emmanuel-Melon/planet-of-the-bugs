515bd3e040688ac18b94a8b1c93b0a4ed5bc8f63
Eman <emmanuelgatwech@gmail.com>
2023-08-06 20:10:16 +0300

    fix: remove duplicate apolloClient instance

--- apps/client/src/lib/graphql/apolloClient.ts
+++ apps/client/src/lib/graphql/apolloClient.ts
@@ -17,65 +17,11 @@ const httpLink = new HttpLink({
 
 type HttpCredentials = "include" | "omit" | "same-origin";
 
-// nuke github client
-const githubHttpLink = ({ Authorization, uri }) => {
-  return new HttpLink({
-    uri,
-    credentials: "same-origin",
-    headers: {
-      Authorization,
-    },
-  });
-};
-
-export const apolloClient = (options) => {
-  return new ApolloClient({
-    cache: new InMemoryCache(),
-    credentials: "include",
-    link: githubHttpLink(options),
-  });
-};
 
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
   credentials: "include",
   link: httpLink,
-});
-
-export class GithubApi {
-  constructor(options) {
-    this.options = options;
-    this.session = null;
-    this.client = null;
-  }
-
-  setSession(session) {
-    this.session = session;
-    this.client = new ApolloClient({
-      cache: new InMemoryCache(),
-      credentials: "include",
-      link: this.createHttpLink(this.options),
-    });
-  }
-
-  getapolloClient() {
-    return this.client;
-  }
-
-  createHttpLink({ Authorization, uri }) {
-    return new HttpLink({
-      uri,
-      credentials: "same-origin",
-      headers: {
-        Authorization: `Bearer ${PUBLIC_GITHUB_ACCESS_TOKEN}`,
-      },
-    });
-  }
-}
-
-export const GITHUB_API = new GithubApi({
-  Authorization: null,
-  uri: PUBLIC_GITHUB_API_ENDPOINT,
 });
 
 export default apolloClient;
