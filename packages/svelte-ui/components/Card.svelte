b26c024169f2b4a5298e6937a9d69c4fc20ab242
Maged Faiz <magedfaiz98@gmail.com>
2023-09-17 14:51:57 +0200

    feat: refactor card

--- packages/svelte-ui/components/Card.svelte
+++ packages/svelte-ui/components/Card.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-  export let bg: String = 'base-100';
+  export let bg: String = 'gray-800/40';
   export let title: String = '';
   export let titleTag: String = 'h2';
   export let bodyVerticalSpacing: number = 2;
   export let height: 'full' | 'fit' = 'full';
 </script>
 
-
-<div class={`card cursor-pointer card-compact bg-${bg} w-full h-${height} shadow`}>
+<div
+  class={`card cursor-pointer card-compact bg-${bg} w-full h-${height} shadow`}
+>
   <div class={`card-body space-y-${bodyVerticalSpacing}`}>
     {#if title}
       {@html `<${titleTag} class="card-title">${title}</${titleTag}>`}
